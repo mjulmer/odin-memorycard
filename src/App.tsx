f2027ce174708d6301a73fba1ed3c82e53dba8f8
M Ulmer <mju9999@gmail.com>
2025-06-10 16:58:33 -0700

    Add initial Vite + ReactTS boilerplate.
    This contains no additions other than the change of the original
    content to "starter text" and the page title. All other changes are
    deletions from the Vite default app.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>
+        Starter text.
+      </p>
+    </>
+  )
+}
+
+export default App
