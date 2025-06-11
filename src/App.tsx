92206d85656aac7e7e021d712e4dd77dde55b9ad
M Ulmer <mju9999@gmail.com>
2025-06-10 21:33:40 -0700

    Add basic logic for shifting game state between 3 core components
    These will eventually get moved into their own files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,57 @@
 import { useState } from "react";
 import "./styles/App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+type GameState = "startScreen" | "inProgress" | "gameEnded";
 
-  return (
-    <>
-      <p>Starter text.</p>
-    </>
-  );
+function App() {
+  const [gameState, setGameState] = useState<GameState>("startScreen");
+  const [highScore, setHighScore] = useState(0);
+  const [currentScore, setCurrentScore] = useState(0);
+
+  if (gameState === "startScreen") {
+    return <StartScreen setGameState={setGameState} />;
+  } else if (gameState === "inProgress") {
+    return <GameScreen currentScore={currentScore} highScore={highScore} />;
+  } else {
+    return <EndScreen currentScore={currentScore} highScore={highScore} />;
+  }
+
+  function StartScreen({
+    setGameState,
+  }: {
+    setGameState: (gameState: GameState) => void;
+  }) {
+    return (
+      <>
+        <button onClick={() => setGameState("inProgress")}>Start game</button>
+      </>
+    );
+  }
+
+  function GameScreen({
+    currentScore,
+    highScore,
+  }: {
+    currentScore: number;
+    highScore: number;
+  }) {
+    return (
+      <>
+        <p>Current score: {currentScore}</p>
+        <p>High score: {highScore}</p>
+      </>
+    );
+  }
+
+  function EndScreen({
+    currentScore,
+    highScore,
+  }: {
+    currentScore: number;
+    highScore: number;
+  }) {
+    return <></>;
+  }
 }
 
 export default App;
